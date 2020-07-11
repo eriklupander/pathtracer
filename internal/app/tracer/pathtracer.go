97a8fb6936660bc16954bd8e365bd89bd0dde289
eriklupander <erik.lupander@gmail.com>
2020-07-12 01:06:18 +0200

    delete dead stuff

--- internal/app/tracer/pathtracer.go
+++ internal/app/tracer/pathtracer.go
@@ -3,7 +3,6 @@ package tracer
 import (
 	"fmt"
 	"github.com/eriklupander/pathtracer/cmd"
-	"github.com/eriklupander/pathtracer/internal/app/camera"
 	canvas2 "github.com/eriklupander/pathtracer/internal/app/canvas"
 	"github.com/eriklupander/pathtracer/internal/app/geom"
 	"github.com/eriklupander/pathtracer/internal/app/scenes"
@@ -21,33 +20,7 @@ var black = geom.NewColor(0, 0, 0)
 var white = geom.NewColor(1, 1, 1)
 
 type PathTracer struct {
-	scene  scenes.Scene
-	camera camera.Camera
-	canvas *canvas2.Canvas
-	jobs   chan *job
-	wg     *sync.WaitGroup
 
-	//total  int
-	//depth  int
-	//
-	//// pixel cache
-	//pointInView     geom.Tuple4
-	//pixel           geom.Tuple4
-	//origin          geom.Tuple4
-	//direction       geom.Tuple4
-	//subVec          geom.Tuple4
-	//shadowDirection geom.Tuple4
-	//
-	//// ray cache
-	//firstRay geom.Ray
-	//
-	//// each renderContext needs to pre-allocate shade-data for sufficient number of recursions
-	////cStack []ShadeData
-	//
-	//// alloc memory for each sample of a given pixel
-	//samples []geom.Tuple4
-	//
-	//rnd *rand.Rand
 }
 
 func (t *PathTracer) Render(sceneFactory func() *scenes.Scene) {
@@ -81,25 +54,7 @@ func (t *PathTracer) Render(sceneFactory func() *scenes.Scene) {
 	fmt.Println("All done")
 	stats := runtime.MemStats{}
 	runtime.ReadMemStats(&stats)
-	//fmt.Printf("Memory: %v ", bytesize.New(float64(stats.Alloc)).String())
-	//fmt.Printf("Mallocs: %v ", stats.Mallocs)
-	//fmt.Printf("Total alloc: %v\n", bytesize.New(float64(stats.TotalAlloc)).String())
-	//fmt.Printf("%v\n", done)
-	//fmt.Printf("XS skipped in group: %v\n", calcstats.Cnt)
-	//fmt.Printf("Transpose calls: %v\n", calcstats.Tpose)
-	//fmt.Printf("Dot calls: %v\n", calcstats.Dots)
-	//fmt.Printf("Cross calls: %v\n", calcstats.Crosses)
-	//fmt.Printf("Normalize calls: %v\n", calcstats.Ns)
-	//
-	//fmt.Println()
-	//fmt.Printf("|%v|%v|%v|%v|%v|%v|\n",
-	//	bytesize.New(float64(stats.Alloc)).String(),
-	//	stats.Mallocs,
-	//	bytesize.New(float64(stats.TotalAlloc)).String(),
-	//	time.Now().Sub(st),
-	//	calcstats.Cnt,
-	//	calcstats.Tpose)
-	//
+
 	fmt.Printf("Finished in %v\n", time.Now().Sub(st))
 	fmt.Printf("Samples taken: %v\n", sampleCount)
 	fmt.Printf("Samples/s: %v\n", float64(sampleCount)/done.Seconds())
